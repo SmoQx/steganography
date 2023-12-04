2cd983aec9202b002fb32ed6daad641fafe52947
smoq <smoq@localhost.localdomain>
2023-12-04 14:06:23 +0100

    created a downloader for files and modified the template of index

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+import pathlib
+from flask import Flask, render_template, request, redirect, url_for, send_file, safe_join
 import os
 from encoder import file_encoder
 from pathlib import Path
@@ -8,7 +9,13 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = 'downloads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 PASSWORD = 'your_password'  # Change this to your desired password
+
+
+# Check if the file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/')
@@ -20,23 +27,34 @@ def hello():
 def upload_file():
     password = request.form.get('password')
 
-    #if password != PASSWORD:
-    #    return 'Incorrect password. Upload not allowed.'
-
     if 'file' not in request.files:
         return redirect(request.url)
 
     file = request.files['file']
 
-
-
-    if file.filename == '':
+    if file.filename == '' or not allowed_file(file.filename):
         return redirect(request.url)
 
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
-        return render_template('/uploaded.html', filename=file, password=password)
+
+        # Encrypt the uploaded file
+        encrypted_file_path = file_encoder(filename)
+
+        return render_template('index.html', filename=encrypted_file_path, password=password)
+
+
+@app.route('/download/<file_name>')
+def download_file(file_name):
+    print("kurwaaa!!!")
+    encrypted_file_path = safe_join(app.config['UPLOAD_FOLDER'], file_name)
+    print(file_name)
+    # Ensure the file exists before attempting to send it
+    if os.path.exists(encrypted_file_path):
+        return send_file(encrypted_file_path, as_attachment=True, download_name=f"{file_name}")
+    else:
+        return render_template("uploaded.html")
 
 
 if __name__ == '__main__':
--- encoder.py
+++ encoder.py
@@ -7,10 +7,8 @@ def file_encoder(file_name: pathlib.Path, password: str = "tekst"):
     if password == "tekst":
         with open(file_name) as file_to_encode:
             file = file_to_encode.read()
-            print(file.split())
         np_loaded = np.loadtxt(file_name)
-        return np_loaded
-
+    return pathlib.Path("dane13.txt")
 
 if __name__ == '__main__':
-    file_encoder(pathlib.Path("./downloads/dane13.txt"))
+    file_encoder(pathlib.Path("dane13.txt"))
