1e3a298b223f29dcf1a51f5c9da4e287e2ff06a2
smoq <smoq@localhost.localdomain>
2023-12-07 12:01:24 +0100

    added functions into decoder and encoder

--- decoder.py
+++ decoder.py
@@ -1,5 +1,33 @@
-def main():
-    pass
+from PIL import Image
+
+
+def extract_text_from_image(image_path):
+    img = Image.open(image_path)
+    binary_data = ""
+
+    for i in range(img.width):
+        for j in range(img.height):
+            pixel = list(img.getpixel((i, j)))
+
+            for color_index in range(3):
+                # Extract the least significant bit from each color channel
+                binary_data += str(pixel[color_index] & 1)
+
+    # Convert binary data to text
+    extracted_text = binary_to_text(binary_data)
+
+    return extracted_text
+
+def binary_to_text(binary_data):
+    text = ''.join(chr(int(binary_data[i:i+8], 2)) for i in range(0, len(binary_data), 8))
+    return text
+
+if __name__ == "__main__":
+    image_path = "path/to/output/image_with_hidden_text.png"
+    
+    extracted_text = extract_text_from_image(image_path)
+
+    print("Extracted Text:", extracted_text)
 
 
 if __name__ == '__main__':
--- encoder.py
+++ encoder.py
@@ -3,6 +3,36 @@ import os
 import pathlib
 
 
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            # Encrypt uppercase letters
+            if char.isupper():
+                result += chr((ord(char) + shift - 65) % 26 + 65)
+            # Encrypt lowercase letters
+            else:
+                result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+
+def encrypt_file(file_path, shift):
+    try:
+        with open(file_path, 'r') as file:
+            plaintext = file.read()
+
+        encrypted_text = caesar_cipher(plaintext, shift)
+
+        with open(file_path, 'w') as file:
+            file.write(encrypted_text)
+
+        print("Encryption successful.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
 def file_encoder(file_name: pathlib.Path, password: str = "tekst"):
     if password == "tekst":
         with open(file_name) as file_to_encode:
@@ -10,5 +40,7 @@ def file_encoder(file_name: pathlib.Path, password: str = "tekst"):
         np_loaded = np.loadtxt(file_name)
     return pathlib.Path("dane13.txt")
 
+
 if __name__ == '__main__':
     file_encoder(pathlib.Path("dane13.txt"))
+
